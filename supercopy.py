dbafbd0f1185b34375efd9579cf1036dfd255f48
ofer kazael <ofer2k@gmail.com>
2021-04-24 10:47:15 +0300

    fix

--- supercopy.py
+++ supercopy.py
@@ -22,25 +22,29 @@ copy_command='robocopy'
 copy_command_flags='/CREATE /B'
 
 def scan_folder_level1(src_folder,dest_folder):
-    fid=open(bat_filename,'w',encoding="utf-8")
-    for p in Path(src_folder).glob('**/*'):
-      if p.is_file() and p.suffix in backup_files_type:
-        d=dest_folder / p.parts[-2] 
-        fid.write(f'{copy_command} "{p}" "{d}" {copy_command_flags} \n')
-        #print(p.name)
-        #breakpoint()
-    fid.close() 
+  fid=open(bat_filename,'w',encoding="utf-8")
+  for p in Path(src_folder).glob('**/*'):
+    if p.is_file() and p.suffix in backup_files_type:
+      d=dest_folder / p.parts[-2] 
+      fid.write(f'{copy_command} "{p}" "{d}" {copy_command_flags} \n')
+      #print(p.name)
+      #breakpoint()
+  fid.close() 
 
 def main(argv):
-    src_folder=join(os.environ['HOMEDRIVE'],os.environ['HOMEPATH'])
-    
-    user_name=os.environ['USERNAME']
-    dest_folder=join(FLAGS.dest,os.environ['COMPUTERNAME'],user_name)
-    for subf in backup_folders_name:
-      f=Path(src_folder) / Path(subf)
-      d=Path(dest_folder) / Path(subf)
-      logging.info(f'copy from: {f} to: {dest_folder}')
-      scan_folder_level1(f,d)
+  if not os.environ['os']== 'Windows_NT':
+    logging.error('run on windows on only')
+    return
+
+  src_folder=join(os.environ['HOMEDRIVE'],os.environ['HOMEPATH'])
+  
+  user_name=os.environ['USERNAME']
+  dest_folder=join(FLAGS.dest,os.environ['COMPUTERNAME'],user_name)
+  for subf in backup_folders_name:
+    f=Path(src_folder) / Path(subf)
+    d=Path(dest_folder) / Path(subf)
+    logging.info(f'copy from: {f} to: {dest_folder}')
+    scan_folder_level1(f,d)
 
 if __name__ == '__main__':
   app.run(main)
