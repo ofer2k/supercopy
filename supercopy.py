a6a07ef90b72ad589df7a621acb5169caf16a1b1
ofer kazael <ofer2k@gmail.com>
2021-04-24 11:25:11 +0300

    fix

--- supercopy.py
+++ supercopy.py
@@ -22,14 +22,17 @@ copy_command='robocopy'
 copy_command_flags='/CREATE /B'
 
 def scan_folder_level1(src_folder,dest_folder):
-  fid=open(bat_filename,'w',encoding="utf-8")
+  fid=open(bat_filename,'a',encoding="utf-8")
+  cc=0
   for p in Path(src_folder).glob('**/*'):
     if p.is_file() and p.suffix in backup_files_type:
       d=dest_folder / p.parts[-2] 
       fid.write(f'{copy_command} "{p}" "{d}" {copy_command_flags} \n')
+      cc+=1
       #print(p.name)
       #breakpoint()
-  fid.close() 
+  fid.close()
+  return cc 
 
 def main(argv):
   if not os.environ['os']== 'Windows_NT':
@@ -40,11 +43,13 @@ def main(argv):
   
   user_name=os.environ['USERNAME']
   dest_folder=join(FLAGS.dest,os.environ['COMPUTERNAME'],user_name)
+  os.remove(bat_filename)
   for subf in backup_folders_name:
     f=Path(src_folder) / Path(subf)
     d=Path(dest_folder) / Path(subf)
-    logging.info(f'copy from: {f} to: {dest_folder}')
-    scan_folder_level1(f,d)
+    logging.info(f'copy from: {f} to: {d}')
+    nfiles=scan_folder_level1(f,d)
+    logging.info(f'number of files {nfiles}')
 
 if __name__ == '__main__':
   app.run(main)
